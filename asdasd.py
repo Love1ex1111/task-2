b86482507c84989d19237c300f182e9a2d29f423
Love1ex1111 <gitlerlove76@gmail.com>
2024-03-20 18:48:34 +0300

    + programm

--- asdasd.py
+++ asdasd.py
@@ -9,3 +9,28 @@ class Point:
 
 point_one = Point(coord_x=2, coord_y=3)
 print(point_one.calculate_distance(coord_x_1=2, coord_y_1=5))
+
+
+
+
+class Bank_akkaynt:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, money):
+        self.balance += money
+
+    def winthdraw(self, money):
+        if money > self.balance:
+            print('Ошибка')
+            raise Exception
+        else:
+            self.balance -= money
+
+    def get_info(self):
+        print(self.balance)
+
+some_account = Bank_akkaynt()
+some_account.deposit(money=4)
+some_account.winthdraw(money=4)
+some_account.get_info()
